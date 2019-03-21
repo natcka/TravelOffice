dca3b7cc1eeffd1f567d2162efc73e2fef421f7f
natcka <n.nawrocka@gmail.com>
2019-03-21 15:32:56 +0100

    komit mesedż

--- src/main/java/travelOffice/models/AbroadTrip.java
+++ src/main/java/travelOffice/models/AbroadTrip.java
@@ -1,7 +1,6 @@
 package travelOffice.models;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public class AbroadTrip extends Trip {
 
--- src/main/java/travelOffice/models/DomesticTrip.java
+++ src/main/java/travelOffice/models/DomesticTrip.java
@@ -1,12 +1,10 @@
 package travelOffice.models;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public class DomesticTrip extends Trip {
 
     private Long ownArrivalDiscount; //w procentach
-    private final boolean domestic = true;
 
     public DomesticTrip(LocalDate start, LocalDate end, String destination, Long price, Long ownArrivalDiscount, boolean domestic) {
         super(start, end, destination, price, domestic);
